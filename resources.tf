3aa816c0e02ca8a31eb327055ea9a80f4416ad83
Khalid <khalid-94@protonmail.com>
2022-06-16 22:51:24 +0100

    Pipeline test

--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test" {
-  ami           = "ami-0bd2099338bc55e6d"
-  instance_type = "t2.micro"
+  ami           =  "ami-0bd2099338bc55e6d"
+  instance_type =  "t2.micro"
 
   tags = {
     environment = "test"
