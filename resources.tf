046e99f73462d29a0b7a4056bd77914ad2d286f6
Khalid <khalid-94@protonmail.com>
2022-06-17 19:39:16 +0100

    New instance

--- resources.tf
+++ resources.tf
@@ -1,8 +1,8 @@
-# resource "aws_instance" "test" {
-#   ami           = "ami-0bd2099338bc55e6d"
-#   instance_type = "t2.micro"
+resource "aws_instance" "test" {
+  ami           = "ami-0bd2099338bc55e6d"
+  instance_type = "t2.micro"
 
-#   tags = {
-#     environment = "test"
-#   }
-# }
+  tags = {
+    environment = "test"
+  }
+}
